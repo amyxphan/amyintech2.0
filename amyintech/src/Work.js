b95a90829722f37aab2003c7ec465d0ce8de2fa8
amyxphan <phan.amy28@gmail.com>
2025-08-30 22:33:13 -0500

    added work content text styling complete

--- amyintech/src/Work.js
+++ amyintech/src/Work.js
@@ -10,30 +10,32 @@ function Work() {
           <section className="Work-screen">
             <div className="Work-container">
               <b className="Work-name">JPMorgan Chase & Co.</b>
-              <div>
-                <b className="Work-subheader">Role</b>
+              <div className="Work-details">
+                <div>
+                  <b className="Work-subheader">Role</b>
+                </div>
+                <p className="Work-description"> Software Engineer</p>
+                <div>
+                  <b className="Work-subheader">Duration</b>
+                </div>
+                <p className="Work-description"> August 2024 - Present</p>
+                <div>
+                  <b className="Work-subheader">Tools</b>
+                </div>
+                <p className="Work-description"> Java, React, Cypress, Gherkin, AWS</p>
+                <div>
+                  <b className="Work-subheader">Team</b>
+                </div>
+                  <p className="Work-description"> Customer Acquisition Experiences (Refer-A-Friend) and Explore Products</p>
+                  <p className="Work-description"> Business Banking Deposits and Account Opening</p>
+                <div>
+                  <b className="Work-subheader">Description</b>
+                </div>
+                <p className="Work-description">
+                  Building and maintaining the Chase existing customer homepage and Refer-a-Friend experience 
+                  on web and mobile platforms; enhancing the user experience for millions of daily customers. 
+                </p>
               </div>
-              <p className="Work-description"> Software Engineer</p>
-              <div>
-                <b className="Work-subheader">Duration</b>
-              </div>
-              <p className="Work-description"> August 2024 - Present</p>
-              <div>
-                <b className="Work-subheader">Tools</b>
-              </div>
-              <p className="Work-description"> Java, React, Cypress, Gherkin, AWS</p>
-              <div>
-                <b className="Work-subheader">Team</b>
-              </div>
-                <p className="Work-description"> Customer Acquisition Experiences (Refer-A-Friend) and Explore Products</p>
-                <p className="Work-description"> Business Banking Deposits and Account Opening</p>
-              <div>
-                <b className="Work-subheader">Description</b>
-              </div>
-              <p className="Work-description">
-                Building and maintaining the Chase existing customer homepage and Refer-a-Friend experience 
-                on web and mobile platforms; enhancing the user experience for millions of daily customers. 
-              </p>
               {/* <a className="Work-hyperlink" href="/work" rel="noopener noreferrer">View Project</a> */}
             </div>
           </section>
@@ -41,29 +43,31 @@ function Work() {
           <section className="Work-screen">
             <div className="Work-container">
               <b className="Work-name">JPMorgan Chase & Co.</b>
-              <div>
-                <b className="Work-subheader">Role</b>
+              <div className="Work-details">
+                <div>
+                  <b className="Work-subheader">Role</b>
+                </div>
+                <p className="Work-description"> Software Engineer Intern</p>
+                <div>
+                  <b className="Work-subheader">Duration</b>
+                </div>
+                <p className="Work-description"> June 2023 - August 2023</p>
+                <div>
+                  <b className="Work-subheader">Tools</b>
+                </div>
+                <p className="Work-description"> Java, React </p>
+                <div>
+                  <b className="Work-subheader">Team</b>
+                </div>
+                  <p className="Work-description"> Business Banking Deposits and Account Opening</p>
+                <div>
+                  <b className="Work-subheader">Description</b>
+                </div>
+                <p className="Work-description">
+                  Developed a backend testing system using Java enabling 100% service automation for customer verification without disrupting user 
+                  experience within 3 months.
+                </p>
               </div>
-              <p className="Work-description"> Software Engineer Intern</p>
-              <div>
-                <b className="Work-subheader">Duration</b>
-              </div>
-              <p className="Work-description"> June 2023 - August 2023</p>
-              <div>
-                <b className="Work-subheader">Tools</b>
-              </div>
-              <p className="Work-description"> Java, React </p>
-              <div>
-                <b className="Work-subheader">Team</b>
-              </div>
-                <p className="Work-description"> Business Banking Deposits and Account Opening</p>
-              <div>
-                <b className="Work-subheader">Description</b>
-              </div>
-              <p className="Work-description">
-                Developed a backend testing system using Java enabling 100% service automation for customer verification without disrupting user 
-                experience within 3 months.
-              </p>
               {/* <a className="Work-hyperlink" href="/work" rel="noopener noreferrer">View Project</a> */}
             </div>
           </section>
