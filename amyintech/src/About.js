538dd26fcc69d29146c1565db1e24149fd69dee1
amyxphan <phan.amy28@gmail.com>
2025-09-06 19:47:34 -0500

    about me content fix

--- amyintech/src/About.js
+++ amyintech/src/About.js
@@ -58,14 +58,20 @@ function About() {
             <section className="About-newspaper">
               <b className="About-name">About Me</b>
               <p className="About-description">
-                I grew up in a low-income, immigrant family near Dallas, TX, where my parents encouraged me to seize every opportunity. As a child, I often stole my dad’s 
-                screwdriver to take apart and reassemble household items. At 12, I entered my first STEM competition and won 1st place in the state of Texas for math, 
-                marking the beginning of my engineering journey. 5 years later, I completed high school in 3 years, balancing leadership activities in 3 different clubs, 
-                attended community college (accumulating 39 college credit hours), and managed our nationally ranked (top 3) varsity girl’s basketball team. I attended 
-                the University of North Texas, earning a B.S. in Computer Engineering. During my time there, I founded the Society of Asian Scientists and Engineers (SASE) 
-                and spent my free time learning computer science concepts beyond my formal studies. I'm currently a Software Engineer at JPMorgan Chase & Co. In my free time, 
-                I enjoy hiking, pilates, playing with my pet hamsters Mochi and Miso, and taking care of my 30+ plants. I am passionate about accessibility and aim to create 
-                innovative solutions that bridge gaps and drive positive change in the tech industry.
+              I’m a Software Engineer at JPMorgan Chase & Co., where I work on the Refer-A-Friend and Explore Products team as a full-stack engineer with a 
+              focus on frontend development and test automation for web and mobile products.
+
+              I grew up in a low-income, immigrant family near Dallas, TX, where curiosity and resilience shaped my path. As a child, I would borrow my dad’s 
+              screwdriver to take apart household items just to see how they worked. At 12, I won 1st place in a statewide math competition—an early spark that 
+              fueled my love for engineering.
+
+              By the time I finished high school (in just three years), I had taken on leadership roles in 3 clubs, earned 39 college credits, and helped 
+              lead our varsity girls’ basketball team to a top 3 national ranking. I went on to earn my B.S. in Computer Engineering at the University of North 
+              Texas, where I also founded the Society of Asian Scientists and Engineers chapter at UNT (SASE UNT). Outside the classroom, I immersed myself in 
+              hackathons and side projects to grow my software skills.
+
+              I’m passionate about accessibility and building innovative solutions that create positive change. Outside of work, you’ll find me exploring 
+              national parks, practicing pilates, caring for 30+ plants, or playing with my two hamsters, Mochi and Miso.
               </p>
             </section>
           </div>
@@ -77,8 +83,6 @@ function About() {
               <p className="About-description">
                 • 2021 Founded SASE at the University of North Texas<br />
                 • 2021-23 President for SASE UNT (2 yrs)<br />
-                • 2023-24 SASE South Regional Coordinator<br />
-                • 2023-24 Vice President - Engineers United (HackUNT)<br />
                 • 2022 UNT New Student Org of the Year<br />
                 • 2022 SASE Outstanding New Chapter<br />
                 • 2022 CodeForGood Hackathon Winner<br />
@@ -86,6 +90,8 @@ function About() {
                 • 2023 SASE South Region Collegiate Star Honorable Mention<br />
                 • 2023 SWE Intern at JPMorgan Chase<br />
                 • 2023 Grace Hopper Celebration Scholar<br />
+                • 2023-24 SASE South Regional Coordinator<br />
+                • 2023-24 Engineers United (HackUNT) Vice President<br />
                 • 2024 WEHack Winner (2nd place L3Harris Challenge)<br />
                 • 2024 Golden Eagle Award Recipient
               </p>
